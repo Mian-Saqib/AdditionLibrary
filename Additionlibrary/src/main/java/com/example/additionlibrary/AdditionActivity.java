bd999ec35db61221dd990eeac0f24f432314edcc
Mian Saqib <saqibrza7@gmail.com>
2021-12-10 12:44:32 +0500

    first commit

---
+++ Additionlibrary/src/main/java/com/example/additionlibrary/AdditionActivity.java
@@ -0,0 +1,87 @@
+package com.example.additionlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AdditionActivity extends AppCompatActivity {
+
+    private EditText first_num_et,second_num_et;
+
+    private Button add_btn,finish_btn;
+
+    Double Result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addition);
+
+        BindViews();
+        AdditionButtonFun();
+        FinishBtnFun();
+
+    }
+
+    private void BindViews()
+    {
+        first_num_et = findViewById(R.id.first_num_et);
+        second_num_et = findViewById(R.id.second_num_et);
+        add_btn = findViewById(R.id.add_btn);
+        finish_btn = findViewById(R.id.finish_btn);
+    }
+
+    private void AdditionButtonFun()
+    {
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Double first_num = Double.valueOf(first_num_et.getText().toString());
+                Double second_num = Double.valueOf(second_num_et.getText().toString());
+
+
+                if (first_num_et.getText().toString().isEmpty() || first_num_et.getText().toString().length()==0)
+                {
+                    first_num_et.setError("Please Enter Some Value");
+                }
+                else if (second_num_et.getText().toString().isEmpty() || second_num_et.getText().toString().length()==0)
+                {
+                    second_num_et.setError("Please Enter Some Value");
+                }
+                else if (first_num_et.getText().toString().isEmpty() && second_num_et.getText().toString().isEmpty())
+                {
+                    first_num_et.setError("Please Enter Some Value");
+                    second_num_et.setError("Please Enter Some Value");
+                }
+
+                else
+                {
+
+                    Result = first_num + second_num;
+
+                }
+
+            }
+        });
+    }
+
+    private void FinishBtnFun()
+    {
+        finish_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Result!=null)
+                {
+                    AdditionLibrary.resultMessage(AdditionActivity.this,Result);
+                    finish();
+                }
+            }
+        });
+
+    }
+
+}
--- Additionlibrary/src/main/java/com/example/additionlibrary/AdditionLibrary.java
+++ Additionlibrary/src/main/java/com/example/additionlibrary/AdditionLibrary.java
@@ -1,4 +1,30 @@
 package com.example.additionlibrary;
 
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
 public class AdditionLibrary {
+
+//    public static void toastMessage(Context c, String message){
+//
+//        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+//
+//    }
+
+    public static void startAdditionActivity(Context context){
+
+        Intent intent = new Intent(context,AdditionActivity.class);
+        context.startActivity(intent);
+    }
+
+
+    public static void resultMessage(Context c, Double result){
+
+        Toast.makeText(c,String.valueOf(result), Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
 }
